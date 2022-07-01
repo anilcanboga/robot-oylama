799a7978838d456ede0d7ade3ee5b0e80395837f
anilcanboga <60179891+anilcanboga@users.noreply.github.com>
2022-07-01 16:03:33 +0300

    development

--- src/components/storeRedux/CompetiterRedux.js
+++ src/components/storeRedux/CompetiterRedux.js
@@ -15,12 +15,16 @@ export const competitersRedux = createSlice({
       let competiterIndex = state.competiters.findIndex(
         (data) => data.id === action.payload
       );
-      let array = [...state.competiters];
-      array[competiterIndex].vote += 1;
-      state.competiters = array.sort(
-        (firstCompetiter, secondCompetiter) =>
-          secondCompetiter.vote - firstCompetiter.vote
-      );
+      // Eğer findIndex -1'den büyük bir sayı dönmez ise böyle bir eleman yok demektir. Yoksa işlem yapmadan devam ederiz.
+      // Aksi halde o index deki data nın vote'unu 1 arttırırız.
+      if (competiterIndex > -1) {
+        let array = [...state.competiters];
+        array[competiterIndex].vote += 1;
+        state.competiters = array.sort(
+          (firstCompetiter, secondCompetiter) =>
+            secondCompetiter.vote - firstCompetiter.vote
+        );
+      }
     },
   },
 });
